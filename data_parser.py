77fe4be337647ddad45e7e1bf66f6e368c3a3937
angrenrusc <53937277+angrenrusc@users.noreply.github.com>
2021-04-17 22:24:15 +0500

    Add files via upload

--- data_parser.py
+++ data_parser.py
@@ -1,12 +1,11 @@
 import dpkt
-import struct
+from sys import argv
 from asn1crypto import x509
 from socket import inet_ntoa
 from re import match
 from pathlib import Path
 from requests import get
 from time import time
-
 
 def tls_multifactory(buf):
     i, n = 0, len(buf)
@@ -22,7 +21,6 @@ def tls_multifactory(buf):
         else: 
             break
         i += len(msg)
-
     return msgs, i
 
 def cert_parse(certificates):
@@ -31,7 +29,6 @@ def cert_parse(certificates):
     if not cert:
         return {}
     str_certs = str(certificates[0])
-    # collecting data
     # length
     certInfo['size'] = len(certificates[0])
     # is self-signed - only one in chain
@@ -53,48 +50,49 @@ def cert_parse(certificates):
     pos = str(cert.public_key).find('b\'', 0, len(str(cert.public_key)))
     public_key_part = str(cert.public_key)[pos+3:-2]
     key_pos = str_certs.find(public_key_part, 0, len(str_certs))
-   
     # fields order
     certInfo['order_1'] = 1 if (sign_pos < issuer_pos < valid_pos < subject_pos < key_pos) else 0 
     certInfo['order_2'] = 1 if (issuer_pos < valid_pos < subject_pos < key_pos < sign_pos) else 0 
-
     return certInfo
 
-def tor_check_1 (certInfo):
-    flags = {}
-    # self-signed check
-    flags['self_signed'] = certInfo['self_signed']
-    # order check
-    flags['order'] = (certInfo['order_1'] | certInfo['order_2'])
-    # issuer check
-    flags['issuer'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.com', certInfo['issuer']) is not None)
-    # subject check and not the same check
-    flags['subject'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.net', certInfo['subject']) is not None) and (not(certInfo['issuer'][:-4] == certInfo['subject'][:-4]))
-    flags['result'] = (int(flags['self_signed']) + int(flags['order']) + int(flags['issuer']) + int(flags['subject'])) * 100 / 4.0
-    return flags
-
-def tor_check_2(certInfo, port):
-    flags = {}
-    # port check
-    flags['port'] = (port in {22, 80, 443, 8001, 8080, 8443, 9000, 9001, 9010, 9901, 20000, 20001, 20002})
-    # subject check
-    flags['subject'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.net', certInfo['subject']) is not None)
-    # size check
-    flags['size'] = (400 < certInfo['size'] < 600)
-    flags['result'] = (int(flags['port']) + int(flags['subject']) + int(flags['size'])) * 100 / 3.0
-    return flags
-
-def tor_check_3(ips):
+def tor_check_ip(ips):
     print('IP check running')
     with open("tor_ips.txt", "r") as f:
         for ip_tor in f:
             for (ip_pcap, port, ip_src) in ips:
                 if ip_tor[:-1] == ip_pcap:
-                    if port in {22, 80, 443, 8080, 8443, 9000, 9001, 9010, 20000, 20002}:
-                        print("User " + ip_src + ' connected to TOR ip ' + ip_pcap)
+                    if port in {9001, 9002, 9003, 9004, 9030, 9031, 9032, 9033, 9100}:
+                        print("User " + ip_src + ' connected to TOR ip ' + ip_pcap + ' with possibility 100%')
+                    elif port in {80, 443}:
+                        print("User " + ip_src + ' connected to TOR ip ' + ip_pcap + ' with possibility 75%')
+
+def tor_check_2(certInfo):
+    flags = {}
+    flags['self_signed'] = certInfo['self_signed']
+    flags['order'] = certInfo['order_1']
+    flags['issuer'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.com', certInfo['issuer']) is not None)
+    flags['subject'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.net', certInfo['subject']) is not None) and (not(certInfo['issuer'][:-4] == certInfo['subject'][:-4]))
+    flags['result'] = str((int(flags['self_signed']) + int(flags['order']) + int(flags['issuer']) + int(flags['subject'])) * 100 / 4) + '%'
+    return flags
+
+def tor_check_3(certInfo, port):
+    flags = {}
+    flags['port'] = (port in {443, 9001, 8443, 22, 80, 8080, 9000, 20000, 20001, 20002})
+    flags['subject'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.net', certInfo['subject']) is not None)
+    flags['size'] = (400 < certInfo['size'] < 600)
+    flags['result'] = str((int(flags['port']) + int(flags['subject']) + int(flags['size'])) * 100 / 3) + '%'
+    return flags
+
+def tor_check_4(certInfo):
+    flags = {}
+    flags['issuer'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.com', certInfo['issuer']) is not None)
+    flags['subject'] = (match(r'www\.[a-zA-Z0-9]{8,20}\.net', certInfo['subject']) is not None) and (not(certInfo['issuer'][:-4] == certInfo['subject'][:-4]))
+    flags['size'] = (400 < certInfo['size'] < 600)
+    flags['self_signed'] = certInfo['self_signed']
+    flags['result'] = str((int(flags['subject']) + int(flags['issuer']) + int(flags['size']) + int(flags['self_signed'])) * 100 / 4) + '%'
+    return flags
 
 def packet_analyse(cap):
-    i = 0
     ips_set = set()
     for timestamp, packet in cap:
         try:
@@ -124,36 +122,44 @@ def packet_analyse(cap):
                 continue
             if hdtype == 11: # certificate
                 certInfo = cert_parse(hd.certificates)
-                print(inet_ntoa(ip.dst), tcp.dport, inet_ntoa(ip.src), tcp.sport) # ips and ports
+                print(inet_ntoa(ip.dst), tcp.dport, inet_ntoa(ip.src), tcp.sport)
                 print(certInfo)
-                print('First check result: ' + str(tor_check_1(certInfo)))
-                print('Second check result: ' + str(tor_check_2(certInfo, tcp.sport)))
+                print('2nd algo result: ' + str(tor_check_2(certInfo)))
+                print('3rd algo result: ' + str(tor_check_3(certInfo, tcp.sport)))
+                print('4th algo result: ' + str(tor_check_4(certInfo)))
                 print()
-    tor_check_3(ips_set)
+    tor_check_ip(ips_set)
 
 def get_ip_file():
     url = "https://www.dan.me.uk/torlist/"
-    #url = "https://raw.githubusercontent.com/gregcusack/tor_classifier/master/tor_ips.txt"
-    response = get(url)
-    open('tor_ips.txt', 'wb').write(response.content)
+    try:
+        response = get(url)
+        open('tor_ips.txt', 'wb').write(response.content)
+    except Exception as e:
+        print(e)
 
 def file_update():
     fips = Path('tor_ips.txt')
     if not(fips.exists()):
         get_ip_file()
-    # check if more than 30 min passed from last file update
     half_hour = 1800
     file_time = fips.stat().st_mtime
     time_now = time()
     if (time_now - file_time > half_hour):
-        print('Updating ip_s file')
+        print('Updating IP file')
         get_ip_file()
 
-def main():
+def main(filename):
     file_update()
-    with open('with_cert.pcap', 'rb') as fp:
-        capture = dpkt.pcap.Reader(fp)
-        packet_analyse(capture)
+    try:
+        with open(filename, 'rb') as fp:
+            capture = dpkt.pcap.Reader(fp)
+            packet_analyse(capture)
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
-    main()
+    if len(argv) != 2:
+        print('Usage: ./data_parser.py <filename>')
+    else:
+        main(str(argv[1]))
